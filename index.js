f03ce4ff6e4dc71802718a359ac19c268f8d3850
dmpty <dmptyy@gmail.com>
2017-10-23 15:55:38 +0800

    update to 0.0.4

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var http = require('http');
 var Router = require('./lib/router');
+var Response = require('./lib/response');
 var Varal = {
     createNew: function (options) {
         var varal = {};
         options = options || {};
-        varal.map = [];
+        varal.routes_map = [];
+        varal.route_groups_map = [];
+        varal.middleware_map = [];
         varal.name = options.name || 'default';
         varal.port = options.port || 8888;
         varal.view_path = options.view_path || 'view';
@@ -14,25 +17,17 @@ var Varal = {
                 var app = {
                     request: request,
                     response: response,
+                    routes_map: varal.routes_map,
+                    middleware_map: varal.middleware_map,
                     view_path: varal.view_path,
                     controller_path: varal.controller_path
                 };
-                var router = Router.createNew(app);
-                router.handle(varal.map);
-            }).listen(varal.port);
-            console.log("Varal Server '" + varal.name + "' has started.");
+                var res = Response.createNew(app);
+                res.handle();
+            }).listen(this.port);
+            console.log("Varal Server '" + this.name + "' has started.");
         };
-        varal.get = function (path, callback) {
-            for (var key in varal.map) {
-                if (varal.map[key].path === path) {
-                    varal.map.splice(key, 1);
-                }
-            }
-            varal.map.push({
-                path: path,
-                callback: callback
-            })
-        };
+        varal.router = Router.createNew(varal);
         return varal;
     }
 };
